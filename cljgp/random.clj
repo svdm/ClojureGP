34baea27f0441bc7c655d6f35951ad6e2d64848f
Stefan van der Meer <stefanvandermeer@gmail.com>
2009-04-01 12:32:04 +0200

    Added rand-fn macro for easier creation of PRNG-enclosing rand fns.

--- cljgp/random.clj
+++ cljgp/random.clj
@@ -20,3 +20,22 @@
   [coll]
   (nth coll (gp-rand-int (count coll)) nil))
 
+(defmacro rand-fn
+  "Macro that takes a method name and an expression returning a new instance of
+  an object, and returns a zero-arg function closing over the object and calling
+  the method on it. The method should return a value between 0.0 and 1.0,
+  similar to (Math/random).
+
+  Intended usage is for creating gp-rand replacement fns with different PRNGs or
+  with user-specified seeds, which can then be used in an experiment's
+  run-config to replace the default time-initialised java.util.Random.
+
+  Example: (rand-fn nextDouble (Random. 123))
+  Becomes: (let [rng (Random. 123)] (fn [] (. rng nextDouble))
+
+  Also useful, for easily creating several seeded PRNG-fns for a threaded
+  experiment: (map #(rand-fn nextDouble (Random. %)) [1234 8472 9000])"
+ [method rng]
+  `(let [r# ~rng]
+     (fn [] (. r# ~method))))
+
