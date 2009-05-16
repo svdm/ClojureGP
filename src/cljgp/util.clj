a23a716c185be592fdcbe68dfec9a7079b18c97d
Stefan van der Meer <stefanvandermeer@gmail.com>
2009-05-16 15:47:31 +0200

    Added EPL licensing information.

--- src/cljgp/util.clj
+++ src/cljgp/util.clj
@@ -1,5 +1,12 @@
+;; Copyright (c) Stefan A. van der Meer. All rights reserved.
+;; The use and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which
+;; can be found in the file epl-v10.html at the root of this distribution. By
+;; using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license. You must not remove this notice, or any other, from
+;; this software.
 
-;;; cljgp.util.clj
+;;; cljgp/util.clj
 
 (ns cljgp.util
   "Utility functions and macros."
