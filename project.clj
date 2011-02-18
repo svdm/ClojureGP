46462c2e52794fd4b36199f1e3ffa38f60526b81
Stefan van der Meer <stefanvandermeer@gmail.com>
2011-02-18 17:12:12 +0100

    Added project.clj with all maven-able deps
    Sadly QN Plot is not among them, will need manual download.

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject cljgp "1.0"
+  :dependencies [[org.clojure/clojure
+                  "1.2.0"]
+                 [org.clojure/clojure-contrib
+                  "1.2.0"]
+
+                 ;; For random generator
+                 [org.uncommons/uncommons-maths
+                  "1.2"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+
+  :repositories {"Java.net Repository" "http://download.java.net/maven/2/"}
+
+  ;; TODO: :main class?
+  )
